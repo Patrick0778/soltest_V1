938a1500ae5fe81af2e370586d47314c3e3cceb9
Patrick0778 <natukuma.patrickjunior@students.mak.ac.ug>
2026-01-11 06:33:12 +0300

    re-test on deployment via the sepolia testnet

---
+++ SimpleStorage_V2.sol
@@ -0,0 +1,36 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+contract SimpleStorage {
+    //Basic types: bool, uint, int, address, bytes
+    uint256 public myfavoriteNumber;// initialized to zero
+
+    //uint256 listOfFavoriteNumbers;
+    struct Person {
+        uint256 favoriteNumber;
+        string name;
+    }
+
+    Person[] public listOfPeople;
+
+    mapping(string => uint256) public nameToFavoritNumber;
+
+    //Person public pat = Person({favoriteNumber: 1, name: "Pat"});
+
+    function store (uint256 _favoriteNumber) public {
+        myfavoriteNumber = _favoriteNumber;
+
+    }
+
+//view, pure - these disallow any modification or updating of state
+    function retrieve () public view returns(uint256) {
+        return myfavoriteNumber;
+
+    }
+
+    function addPerson(string memory _name, uint256 _favoriteNumber) public {
+        listOfPeople.push(Person({favoriteNumber: _favoriteNumber, name: _name}));
+        nameToFavoritNumber[_name] = _favoriteNumber;
+    }
+
+}
